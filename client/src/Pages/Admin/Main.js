b09b78cb5ba9130651ca0036d91d6d8e5fc7d3d2
Shivansh Goel <shivanshgoel21@gmail.com>
2021-08-31 20:26:55 +0530

    Admin: ManageCustomers + ManageOrders table

--- client/src/Pages/Admin/Main.js
+++ client/src/Pages/Admin/Main.js
@@ -1,14 +1,49 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
 // components
 import AppBarCustom from '../../Components/AppBarCustom'
+import DisplayCount from '../../Components/DispalyCount/DisplayCount'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    marginTop: 20,
+    marginLeft: 20,
+    marginRight: 20
+  },
+  displayCount: {
+  
+  }
+}));
 
 function Home() {
-    return (
-        <div>
-            <AppBarCustom/>
-        </div>
-    )
+  const classes = useStyles();
+
+  return (
+    <div>
+      <AppBarCustom />
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid className={classes.displayCount} item xs={12} sm={3}>
+            <DisplayCount count={1000} text={'Customers'} />
+          </Grid>
+          <Grid className={classes.displayCount} item xs={12} sm={3}>
+            <DisplayCount count={200} text={'Orders'} />
+          </Grid>
+          <Grid className={classes.displayCount} item xs={12} sm={3}>
+            <DisplayCount count={576} text={'Products'} />
+          </Grid>
+          <Grid className={classes.displayCount} item xs={12} sm={3}>
+            <DisplayCount count={25} text={'Claims'} />
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  )
 }
+
+
 
 export default Home
