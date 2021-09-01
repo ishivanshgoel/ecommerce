23727f07fe977bc594fe66d593ecb8a872dde79a
Shivansh Goel <shivanshgoel21@gmail.com>
2021-09-01 17:41:21 +0530

    AddProduct

--- client/src/Pages/Admin/AddProduct.js
+++ client/src/Pages/Admin/AddProduct.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import FormControl from '@material-ui/core/FormControl'
+import Input from '@material-ui/core/Input'
+import InputLabel from '@material-ui/core/InputLabel'
+import { FormHelperText } from '@material-ui/core'
 
 // components
 import AppBarCustom from '../../Components/AppBarCustom'
@@ -6,8 +11,38 @@ import AppBarCustom from '../../Components/AppBarCustom'
 function AddProduct() {
     return (
         <div>
-            <AppBarCustom/>
-            Add Product
+            <AppBarCustom text={'Add Product'} />
+            <Grid container spacing={3}>
+                <Grid item xs={12} sm={6}>
+                    Image Here
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <FormControl>
+                        <InputLabel htmlFor="my-input">Name</InputLabel>
+                        <Input id="my-input" aria-describedby="my-helper-text" style={{width: 500}}/>
+                    </FormControl>
+                    <FormControl>
+                        <InputLabel htmlFor="my-input">Price</InputLabel>
+                        <Input id="my-input" aria-describedby="my-helper-text" style={{width: 500}}/>
+                    </FormControl>
+                    <FormControl>
+                        <InputLabel htmlFor="my-input">Quantity</InputLabel>
+                        <Input id="my-input" aria-describedby="my-helper-text" style={{width: 500}}/>
+                    </FormControl>
+                    <FormControl>
+                        <InputLabel htmlFor="my-input"></InputLabel>
+                        <Input id="my-input" aria-describedby="my-helper-text" style={{width: 500}}/>
+                    </FormControl>
+                    <FormControl>
+                        <InputLabel htmlFor="my-input">Name</InputLabel>
+                        <Input id="my-input" aria-describedby="my-helper-text" style={{width: 500}}/>
+                    </FormControl>
+                    <FormControl>
+                        <InputLabel htmlFor="my-input">Name</InputLabel>
+                        <Input id="my-input" aria-describedby="my-helper-text" style={{width: 500}}/>
+                    </FormControl>
+                </Grid>
+            </Grid>
         </div>
     )
 }
