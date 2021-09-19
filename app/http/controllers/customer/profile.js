2ddf4ed5da812629f168b0eba49d1f61e15f2262
Shivansh Goel <shivanshgoel21@gmail.com>
2021-09-19 23:52:10 +0530

    profile.js: customer profile service

--- app/http/controllers/customer/profile.js
+++ app/http/controllers/customer/profile.js
@@ -1,2 +1,35 @@
-// view profile information of customer
-// update profile of customer
+const express = require('express')
+const router = express.Router()
+const User = require('../../../models/user')
+
+/**get user profile information */
+router.get('/info', async(req, res, next)=>{
+    try{
+
+        const userId = '111' // get from request payload
+        const user = await User.findOne({ _id: userId }).exec()
+        res.json({
+            data: user,
+            message: "success"
+        })
+
+    } catch(err){
+        next(err)
+    }
+})
+
+/**update user profile */
+router.put('/info', async(req, res, next)=>{
+    try{
+
+        const userId = '111' // get from request payload
+        //TODO
+        res.json({
+            data: [],
+            message: "success"
+        })
+
+    } catch(err){
+        next(err)
+    }
+})
