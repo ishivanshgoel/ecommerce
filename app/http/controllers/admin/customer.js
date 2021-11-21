cfdc32b0ccab22aa7b99cc12d181b4353b322134
Shivansh Goel <shivanshgoel21@gmail.com>
2021-11-21 15:40:39 +0530

    admin customer rouute

--- app/http/controllers/admin/customer.js
+++ app/http/controllers/admin/customer.js
@@ -1,2 +1,43 @@
 // get list of all customers
 // update information of all customers
+
+const express = require('express')
+const router = express.Router()
+const User = require('../../../models/user')
+
+/** get all orders */
+router.get('/', async(req, res, next)=>{
+    try{
+
+        let users = await User.find({}).exec()
+
+        res.json({
+            data: users,
+            message: "success"
+        })
+
+    } catch(err){
+        next(err)
+    }
+})
+
+/** update a particular user  */
+router.patch('/', async (req, res, next) => {
+    try {
+
+        if(!req.body) throw new Error('Bad Request')
+
+        let updateObject = req.body
+        let id = req.body.id
+        User.updateOne({_id  : ObjectId(id)}, {$set: updateObject});
+        res.json({
+            message: "success"
+        })
+        
+    } catch (err) {
+        next(err)
+    }
+
+})
+
+module.exports = router
--- client/src/Pages/Admin/Login.js
+++ client/src/Pages/Admin/Login.js
@@ -10,6 +10,7 @@ import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import { useHistory } from 'react-router';
 
 function Copyright() {
     return (
@@ -61,10 +62,13 @@ export default function Login() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
+    const history = useHistory()
+
     function handleLogin(event){
         event.preventDefault();
         console.log('Email ', email)
         console.log('Password ', password)
+        history.push('/admin/dashboard')
     }
 
     return (
